8be486856ee07f1e34a5ff344b63d97528630a14
ShibaversX <125780097+ShibaversX@users.noreply.github.com>
2023-10-27 03:54:53 +0200

    Add files via upload
    Everything has been translated into French

---
+++ fr/emails/common.js
@@ -0,0 +1,6 @@
+/* eslint-disable quotes */
+/* eslint-disable max-len */
+module.exports = {
+  supportMsg: `<p>Nous sommes là pour vous ! Si vous avez des préoccupations, notre <a href="https://taikai.network/en/support">Service client</a> est à votre disposition pour vous aider.</p>`,
+  expirationMsg: `<p>Vous avez 7 jours pour terminer votre processus d'activation. Si vous rencontrez des problèmes, copiez cette URL et collez-la dans votre navigateur :</p>`,
+};
---
+++ fr/emails/index.js
@@ -0,0 +1,254 @@
+const subjects = {
+  'account-validated': {
+    subject: 'TAIKAI - Votre compte est maintenant validé',
+    preheader: 'Votre compte TAIKAI est maintenant validé, et vous êtes libre d'utiliser toutes les fonctionnalités disponibles sur notre plateforme.',
+  },
+  'allocate-received': {
+    subject: 'TAIKAI - Vous avez reçu des VKAIs !',
+    preheader: 'Vous avez reçu {{amount}} VKAIs pour soutenir des projets',
+  },
+  'back-success': {
+    subject: 'TAIKAI - Vous venez de soutenir un projet !',
+    preheader: 'Soutien de {{amount}} VKAIs à {{name}} confirmé',
+  },
+  'challenge-transfer-received': {
+    subject: 'TAIKAI - Vous avez reçu des fonds pour soutenir des projets',
+    preheader: 'Transfert de {{amount}} VKAIs en {{challengeName}} de {{from}} reçu',
+  },
+  'challenge-transfer-sent': {
+    subject: 'TAIKAI - Vous avez envoyé des fonds pour soutenir des projets',
+    preheader: 'Transfert de {{amount}} VKAIs en {{challengeName}} à {{to}} confirmé',
+  },
+  'received-dividend': {
+    subject: 'TAIKAI - Vous avez reçu une récompense',
+    preheader: 'Récompense de {{amount}} TKAIs reçue pour avoir été membre du jury',
+  },
+ 'back-failed': {
+    subject: 'TAIKAI - Échec du soutien de votre projet !',
+    preheader: 'Soutien de {{amount}} TKAIs à {{projectName}} échoué',
+},
+'challenge-transfer-sent-failed': {
+    subject: 'TAIKAI - Échec du transfert de vos TKAIs',
+    preheader: 'Transfert de {{amount}} TKAIs en {{challengeName}} vers {{to}} échoué',
+},
+'transfer-sent-failed': {
+    subject: 'TAIKAI - Échec du transfert de fonds',
+    preheader: 'Transfert global de {{amount}} TKAIs vers {{to}} échoué',
+},
+'market-place-order': {
+    subject: 'TAIKAI - Commande sur la Marketplace TAIKAI',
+    preheader: 'Merci pour votre commande. Nous avons bien reçu votre commande.',
+},
+'transfer-sent': {
+    subject: 'TAIKAI - Vous avez envoyé des TKAIs !',
+    preheader: 'Vous avez soumis avec succès une nouvelle organisation à TAIKAI.',
+},
+'transfer-received': {
+    subject: 'TAIKAI - Vous avez reçu des TKAIs !',
+    preheader: 'Transfert global de {{amount}} TKAIs depuis {{from}} reçu',
+},
+'deposit-received': {
+    subject: 'TAIKAI - Vous avez reçu des TKAIs !',
+    preheader: 'Dépôt de {{amount}} TKAIs reçu',
+},
+'invite-to-project': {
+    subject: 'TAIKAI - Vous avez été invité à rejoindre l'équipe {{projectName}}',
+    preheader: 'Vous avez reçu une invitation pour rejoindre l'équipe {{projectName}} sur le défi {{challengeName}}',
+},
+'invite-to-project-unknown': {
+    subject: 'TAIKAI - Vous avez été invité à rejoindre l'équipe {{projectName}}',
+    preheader: 'Vous avez reçu une invitation pour rejoindre l'équipe {{projectName}} sur le défi {{challengeName}}',
+},
+  'registered-as-crowd-backer': {
+    subject: 'Vous avez été enregistré en tant que jury',
+    preheader: 'Vous avez été enregistré en tant que jury',
+},
+'registered-as-guest-jury': {
+    subject: 'Vous avez été enregistré en tant que jury invité',
+    preheader: 'Vous avez été enregistré en tant que jury invité',
+},
+'register-as-mentor': {
+    subject: 'TAIKAI - Vous êtes officiellement Mentor sur {{challengeName}}',
+    preheader: 'TAIKAI - Vous êtes officiellement Mentor sur {{challengeName}}',
+},
+'registered-as-innovator': {
+    subject: 'TAIKAI - Vous êtes enregistré en tant que participant',
+    preheader: 'Vous êtes enregistré en tant que participant sur {{challengeName}}',
+},
+'participant-registration-accepted': {
+    subject: 'TAIKAI - Participation acceptée pour {{challengeName}}',
+    preheader: 'Vous êtes officiellement participant sur {{challengeName}}',
+},
+'challenge-registration-review': {
+    subject: 'TAIKAI - Inscription d'innovateur en cours d'examen pour {{challengeName}}',
+    preheader:
+      'Votre inscription en tant que participant pour {{challengeName}} par {{organizationName}} est en cours d'examen',
+},
+'challenge-next-step-project': {
+    subject: 'TAIKAI - {{challengeName}} - Votre candidature progresse',
+    preheader: 'TAIKAI - {{challengeName}} - Votre candidature progresse',
+},
+organization: {
+  subject: 'TAIKAI - Vous avez reçu une annonce',
+  preheader: 'Mise à jour de l'organisation',
+},
+'added-as-project-member': {
+  subject: 'TAIKAI - Vous faites partie d'une équipe !',
+  preheader: 'Vous avez été ajouté à {{projectName}} en tant que membre du projet',
+},
+'friend-referral-confirmed': {
+  subject: 'TAIKAI - votre ami {{to}} a confirmé votre parrainage',
+  preheader: 'Étant donné que vous avez terminé un autre parrainage, vous recevrez {{amount}} TKAIs',
+},
+mail: {
+  subject: 'TAIKAI - Nouveau message reçu sur le fil {{topic}}',
+  preheader: 'Vous avez un message',
+},
+general: {
+  subject: 'TAIKAI - Vous avez reçu une annonce',
+  preheader: 'Annonce générale',
+},
+'challenge-announcement': {
+  subject: 'TAIKAI - Vous avez reçu une annonce de {{challengeName}}',
+  preheader: 'Vous avez reçu une annonce de {{challengeName}}',
+},
+'challenge-feedback': {
+  subject: 'TAIKAI - Nous avons besoin de votre avis',
+  preheader: 'Veuillez soumettre votre avis sur {{challengeName}}',
+},
+'challenge-update': {
+  subject: 'TAIKAI - Mise à jour de {{challengeName}} | {{title}}',
+  preheader: 'Vous avez reçu une annonce de {{challengeName}}',
+},
+'candidate-rejected': {
+  subject: 'TAIKAI - {{challengeName}} - Votre candidature a été rejetée',
+  preheader: 'TAIKAI - {{challengeName}} - Votre candidature a été rejetée',
+},
+'candidate-passed': {
+  subject: 'TAIKAI - {{challengeName}} - Votre candidature avance',
+  preheader: 'TAIKAI - {{challengeName}} - Votre candidature avance',
+},
+'organization-submitted': {
+  subject: 'TAIKAI - {{organizationName}} soumis à TAIKAI',
+  preheader: 'Candidature reçue',
+},
+'challenge-submitted': {
+  subject: 'TAIKAI - {{challengeName}} soumis à TAIKAI',
+  preheader: 'Challenge soumis pour examen',
+},
+'organization-in-review': {
+  subject: 'TAIKAI - {{organizationName}} va être examiné',
+  preheader: 'Vous avez soumis avec succès une nouvelle organisation à TAIKAI',
+},
+'challenge-in-review': {
+  subject: 'TAIKAI - {{challengeName}} va être examiné',
+  preheader: 'Vous avez soumis avec succès un nouveau défi à TAIKAI',
+},
+'organization-published': {
+  subject: 'TAIKAI - {{organizationName}} a été publié',
+  preheader: 'Partagez la page de votre organisation au sein de votre organisation, de vos partenaires et de vos amis',
+},
+'pops-minted': {
+  subject: 'TAIKAI - Les PoPs de {{challengeName}} ont été créés avec succès',
+  preheader: 'Vous avez créé avec succès de nouveaux PoPs',
+},
+'feedback-pop-minted': {
+  subject: 'TAIKAI - Les PoPs de feedback de {{challengeName}} ont été créés avec succès',
+  preheader: 'Vous avez créé avec succès de nouveaux PoPs',
+},
+'friend-referral': {
+  subject: 'TAIKAI - Vous avez reçu une recommandation d'ami pour vous inscrire sur TAIKAI',
+  preheader: 'Vous avez reçu une recommandation d'ami de {{from}} pour vous inscrire sur TAIKAI',
+},
+'invite-to-organization': {
+  subject: 'Vous avez été invité à rejoindre une organisation',
+  preheader: 'Vous avez été invité à faire partie de {{organizationName}} sur TAIKAI.',
+},
+'invite-to-organization-unknown': {
+  subject: 'TAIKAI - Vous avez été invité à rejoindre une organisation',
+  preheader: 'Vous avez été invité à faire partie de {{organizationName}} sur TAIKAI',
+},
+'invite-to-jury': {
+  subject: 'TAIKAI - Vous avez été invité à devenir membre du jury',
+  preheader: 'Vous avez été invité à choisir les meilleurs projets sur {{challengeName}} sur TAIKAI',
+},
+'invite-to-jury-unknown': {
+  subject: 'TAIKAI - Vous avez été invité à devenir membre du jury',
+  preheader: 'Vous avez été invité à choisir les meilleurs projets sur {{challengeName}} sur TAIKAI',
+},
+'invite-to-participant': {
+  subject: 'TAIKAI - Vous avez été invité à devenir un participant',
+  preheader: 'Êtes-vous prêt à relever le défi et à participer à {{challengeName}} ?',
+},
+'invite-to-participant-unknown': {
+  subject: 'TAIKAI - Vous avez été invité à devenir un participant',
+  preheader: 'Êtes-vous prêt à relever le défi et à participer à {{challengeName}} ?',
+},
+'invite-to-mentor': {
+  subject: 'TAIKAI - Vous avez été invité à devenir un mentor sur {{challengeName}}',
+  preheader: 'TAIKAI - Vous avez été invité à devenir un mentor sur {{challengeName}}',
+},
+'invite-to-mentor-unknown': {
+  subject: 'TAIKAI - Nous avons un rôle important pour vous',
+  preheader: 'TAIKAI - Nous avons un rôle important pour vous',
+},
+'challenge-report': {
+  subject: 'TAIKAI - Fichiers de présentation',
+  preheader: 'Un fichier de présentation du challenge {{challengeName}} a été généré',
+},
+'register-as-jury': {
+  subject: 'Vous avez été invité à devenir membre du jury',
+  preheader: 'Souhaitez-vous choisir les projets les plus innovants sur {{challengeName}} ?',
+},
+'register-as-participant': {
+  subject: 'Vous avez été invité à devenir un participant',
+  preheader: 'Êtes-vous prêt à créer une solution exceptionnelle pour {{challengeName}} ?',
+},
+  'delete-account': {
+    subject: 'TAIKAI - Votre compte a été supprimé',
+    preheader: 'Votre compte et vos données privées ont été supprimés de nos systèmes',
+},
+welcome: {
+    subject: 'Bienvenue sur Taikai',
+    preheader:
+        'Bienvenue dans votre aventure sur TAIKAI ! Pour commencer, veuillez cliquer sur le lien pour activer votre compte.',
+},
+'update-user-email': {
+    subject: 'Veuillez confirmer votre nouveau courriel sur TAIKAI',
+    preheader: 'Veuillez confirmer que c'est la nouvelle adresse e-mail de votre compte TAIKAI.',
+},
+'reset-password': {
+    subject: 'Réinitialisez votre mot de passe TAIKAI',
+    preheader:
+        'Avez-vous perdu votre mot de passe sur taikai.network ? Pour réinitialiser votre mot de passe, veuillez cliquer sur le lien.',
+},
+'confirm-and-reset-password': {
+    subject: 'Veuillez activer votre compte et réinitialiser votre mot de passe',
+    preheader:
+        'Il semble que vous ayez réinitialisé votre mot de passe, mais vous n'avez pas encore activé votre compte.',
+},
+'challenge-published': {
+    subject: 'TAIKAI - {{challengeName}} a été publié',
+    preheader: 'Promouvez la page de votre défi auprès de votre public, de vos partenaires et de vos amis',
+},
+'claim-nft': {
+    subject: 'Réclamez votre NFT du hackathon {{challengeName}}',
+    preheader: 'Nous avons un NFT exclusif basé sur les données de votre hackathon.',
+},
+'withdraw-success': {
+    subject: "La demande de retrait a été traitée",
+    preheader: "La demande de retrait a été traitée",
+},
+'two-fa-locked-out': {
+    subject: '{{username}} a été temporairement verrouillé',
+    preheader: "Nombre maximum de tentatives 2FA dépassé",
+},
+'two-fa-locked-out-warning': {
+    subject: 'Votre compte TAIKAI a été temporairement verrouillé',
+    preheader: "Nombre maximum de tentatives 2FA dépassé",
+},
+};
+
+module.exports = {
+  subjects,
+};
