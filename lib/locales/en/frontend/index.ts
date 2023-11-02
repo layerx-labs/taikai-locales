74e4717e9c9d81d0aae408977bcded3c332d7ce4
JoseMarshall <hebojosemar@gmail.com>
2023-11-02 12:05:18 +0000

    chore: test push

--- lib/locales/en/frontend/index.ts
+++ lib/locales/en/frontend/index.ts
@@ -1,26 +1,5 @@
 // ////////////////////////////////////////////////////////////////
-// DO NOT MODIFY THIS FILE DIRECTLY. IT IS GENERATED.
-// ////////////////////////////////////////////////////////////////
+    // DO NOT MODIFY THIS FILE DIRECTLY. IT IS GENERATED.
+    // ////////////////////////////////////////////////////////////////
 
-export default {
-  'alert-messages': '',
-  'api-messages': '',
-  auth: '',
-  challenges: '',
-  common: '',
-  community: '',
-  countries: '',
-  dashboard: '',
-  'form-messages': '',
-  'hiring-challenge': '',
-  home: '',
-  homepage: '',
-  live: '',
-  navigation: '',
-  organizations: '',
-  product: '',
-  rankings: '',
-  settings: '',
-  testimonials: '',
-  user: '',
-};
+  export default {"alert-messages":"","api-messages":"","auth":"","challenges":"","common":"","community":"","countries":"","dashboard":"","form-messages":"","hiring-challenge":"","home":"","homepage":"","live":"","navigation":"","organizations":"","product":"","rankings":"","settings":"","testimonials":"","user":""};
