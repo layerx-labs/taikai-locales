c2cd4121c73c1bb8fa4746e8f7bfeca72fa8698e
Rtandrew Paul <anderson_rodax@hotmail.com>
2024-05-06 10:41:21 +0100

    [TKAI-3585] Check unused locales (#30)
    * wip
    
    * wip
    
    * wip: function to delete message
    
    * update: revert deleted messages
    
    * feat: add `checkUnusedMessage` function
    
    * feat: delete unused messages
    
    * feat: add missin messages
    
    * fix: delete deeply nested keys
    
    * chore: generate types
    
    * update(i18n): recover `comments` deleted translations
    
    * update: delete unused messages
    
    * chore: lint
    
    ---------
    
    Co-authored-by: Henrique Macedo <2805206+ickas@users.noreply.github.com>

---
+++ scripts/check-unused-translations.ts
@@ -0,0 +1,58 @@
+import fs from 'fs';
+import { globby } from 'globby';
+
+/** Delete deeply nested key from an object */
+function deleteKey(obj: object, path: string) {
+  const keys = path.split('.');
+  let currentObj = obj;
+
+  for (let i = 0; i < keys.length - 1; i++) {
+    if (!currentObj || typeof currentObj !== 'object') {
+      return;
+    }
+    currentObj = currentObj[keys[i]];
+  }
+
+  const lastKey = keys[keys.length - 1];
+  if (currentObj && typeof currentObj === 'object' && lastKey in currentObj) {
+    delete currentObj[lastKey];
+  }
+}
+
+// An array containing which message paths
+// to delete from the i18n files
+export const messagesToDeleteFromLocales = [
+  'label.current_email',
+  'error.minimum_amount_insufficient',
+  'connected_accounts.connect_metamask_wallet_description',
+];
+
+const checkUnusedTranslationMessages = async () => {
+  console.log('Checking and deleting unused translation messages');
+
+  const notReadingDir = [
+    'lib/**/frontend/**',
+    '!**.ts',
+    '!**/api-messages.json',
+    '!**/countries.json',
+  ];
+  const files = await globby(notReadingDir);
+
+  for (const filePath of files) {
+    const fileWithMessages = await import(`../${filePath}`);
+
+    // use the ['default'] because of how the "unamed import" works
+    const messagesToDeleteMap: Record<string, number> = { ...fileWithMessages['default'] };
+
+    // dynamically delete deeply nested objects
+    messagesToDeleteFromLocales.forEach((message) => deleteKey(messagesToDeleteMap, message));
+
+    // rewrite back the file with the objects removed
+    fs.writeFile(filePath, JSON.stringify(messagesToDeleteMap, null, 2), (err) => {
+      if (err) console.log(`Error writing back the file ${filePath}`, err);
+    });
+  }
+
+  console.log('Successfuly verified the unused messages ✅');
+};
+(async () => await checkUnusedTranslationMessages())();
